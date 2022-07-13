ba16ae70168abbc2ba0286e820f05e1db6fdab6c
Alex Westbrook <alex.e.westbrook@gmail.com>
2022-07-13 11:34:50 +0200

    update pipeline

--- bash_pipeline.sh
+++ bash_pipeline.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+scripts_dir='.'
+data_dir='../shared_folder'
+writing_dir='../'
+bbmap_dir='../bbmap'
+
 #### downloads
+data='NONO'
+# IP
+IP_file1='ENCFF767SQO'
+IP_file2='ENCFF527AMZ'
+wget https://www.encodeproject.org/files/$IP_file1/@@download/$IP_file1.fastq.gz -P $data_dir/$data/raw_data/IP
+wget https://www.encodeproject.org/files/$IP_file2/@@download/$IP_file2.fastq.gz -P $data_dir/$data/raw_data/IP
+# Control
+Control_file1='ENCFF002DUF'
+Control_file2='ENCFF002EGX'
+wget https://www.encodeproject.org/files/$Control_file1/@@download/$Control_file1.fastq.gz -P $data_dir/$data/raw_data/Control
+wget https://www.encodeproject.org/files/$Control_file2/@@download/Control_file2.fastq.gz -P $data_dir/$data/raw_data/Control
+# Unzip
+gunzip $data_dir/$data/raw_data/*/*.gz
 
-# data='NONO'
-# # IP
-# IP_file1='ENCFF767SQO'
-# IP_file2='ENCFF527AMZ'
-# wget https://www.encodeproject.org/files/$IP_file1/@@download/$IP_file1.fastq.gz -P $data/raw_data/IP
-# wget https://www.encodeproject.org/files/$IP_file2/@@download/$IP_file2.fastq.gz -P $data/raw_data/IP
-# # Control
-# Control_file1='ENCFF002DUF'
-# Control_file2='ENCFF002EGX'
-# wget https://www.encodeproject.org/files/$Control_file1/@@download/$Control_file1.fastq.gz -P $data/raw_data/Control
-# wget https://www.encodeproject.org/files/$Control_file2/@@download/Control_file2.fastq.gz -P $data/raw_data/Control
-# # Unzip
-# gunzip $data/raw_data/*/*.gz
 
-
-#### dedupe
+#### remove optical and PCR duplicates from fastq
 data='CENPT'
 label1='IP'
 label2='Control'
@@ -27,97 +31,97 @@ access3='ENCFF494PEG'
 access4='ENCFF923IOZ'
 paired_end=false
 
-in1=$data/raw_data/$label1/$access1.fastq
-in2=$data/raw_data/$label1/$access2.fastq
-out1=$data/raw_data/$label1/$access1'_deduped'.fastq
-out2=$data/raw_data/$label1/$access2'_deduped'.fastq
+in1=$data_dir/$data/raw_data/$label1/$access1.fastq
+in2=$data_dir/$data/raw_data/$label1/$access2.fastq
+out1=$data_dir/$data/raw_data/$label1/$access1'_deduped'.fastq
+out2=$data_dir/$data/raw_data/$label1/$access2'_deduped'.fastq
 
-in3=$data/raw_data/$label2/$access3.fastq
-in4=$data/raw_data/$label2/$access4.fastq
-out3=$data/raw_data/$label2/$access3'_deduped'.fastq
-out4=$data/raw_data/$label2/$access4'_deduped'.fastq
+in3=$data_dir/$data/raw_data/$label2/$access3.fastq
+in4=$data_dir/$data/raw_data/$label2/$access4.fastq
+out3=$data_dir/$data/raw_data/$label2/$access3'_deduped'.fastq
+out4=$data_dir/$data/raw_data/$label2/$access4'_deduped'.fastq
 if [ $paired_end = true ]
 then
-    bash bbmap/clumpify.sh in=$in1 in2=$in2 out=$out1 out2=$out2 -subs=0 dedupe
-    bash bbmap/clumpify.sh in=$in3 in2=$in4 out=$out3 out2=$out4 -subs=0 dedupe
+    bash $bbmap_dir/clumpify.sh in=$in1 in2=$in2 out=$out1 out2=$out2 -subs=0 dedupe
+    bash $bbmap_dir/clumpify.sh in=$in3 in2=$in4 out=$out3 out2=$out4 -subs=0 dedupe
 else
-    bash bbmap/clumpify.sh in=$in1 out=$out1 -subs=0 dedupe
-    bash bbmap/clumpify.sh in=$in2 out=$out2 -subs=0 dedupe
-    bash bbmap/clumpify.sh in=$in3 out=$out3 -subs=0 dedupe
-    bash bbmap/clumpify.sh in=$in4 out=$out4 -subs=0 dedupe
+    bash $bbmap_dir/clumpify.sh in=$in1 out=$out1 -subs=0 dedupe
+    bash $bbmap_dir/clumpify.sh in=$in2 out=$out2 -subs=0 dedupe
+    bash $bbmap_dir/clumpify.sh in=$in3 out=$out3 -subs=0 dedupe
+    bash $bbmap_dir/clumpify.sh in=$in4 out=$out4 -subs=0 dedupe
 fi
 
 
 #### dataset creation
 
-# # extract fastq
-# data='NONO'
-# IP_file1='ENCFF767SQO.fastq'
-# IP_file2='ENCFF527AMZ.fastq'
-# Control_file1='ENCFF002DUF.fastq'
-# Control_file2='ENCFF002EGX.fastq'
-# python fastq_to_npz.py $data/raw_data/IP_reads $data/raw_data/IP/$IP_file1 $data/raw_data/IP/$IP_file2
-# python fastq_to_npz.py $data/raw_data/Control_reads $data/raw_data/Control/$Control_file1 $data/raw_data/Control/$Control_file2
+# extract fastq
+data='NONO'
+IP_file1='ENCFF767SQO.fastq'
+IP_file2='ENCFF527AMZ.fastq'
+Control_file1='ENCFF002DUF.fastq'
+Control_file2='ENCFF002EGX.fastq'
+python $scripts_dir/fastq_to_npz.py $writing_dir/$data/raw_data/IP_reads $data_dir/$data/raw_data/IP/$IP_file1 $data_dir/$data/raw_data/IP/$IP_file2
+python $scripts_dir/fastq_to_npz.py $writing_dir/$data/raw_data/Control_reads $data_dir/$data/raw_data/Control/$Control_file1 $data_dir/$data/raw_data/Control/$Control_file2
 
 
 # build dataset
-# data='reads_test_mutasome'
+data='CENPT'
+dataset='dataset'
+python $scripts_dir/build_dataset.py -ip $data_dir/$data/raw_data/IP_reads.npz -ctrl $data_dir/$data/raw_data/Control_reads.npz -out $data_dir/$data/$dataset -bal -20
+
+
+
+
+### Training and evaluating
+
+data='CENPT'
+dataset='dataset'
+model_name='model_inception'
+architecture='Inception.py'
+# create output directory
+mkdir $writing_dir/$data/Trainedmodels/$model_name/
+mkdir $writing_dir/$data/results/$model_name/
+# train model
+python $scripts_dir/Train_model.py -arch $architecture -d $data_dir/$data/$dataset.npz -out $writing_dir/$data/Trainedmodels/$model_name/
+
+
+# save test predictions
+# data='Bombyx'
 # dataset='dataset'
-# python build_dataset.py -ip $data/raw_data/IP_reads.npz -ctrl $data/raw_data/Control_reads.npz -out $data/$dataset -bal -20
+# model_name='model_Yann_original2'
+res_name='distrib_'$model_name'_test'
+python $scripts_dir/evaluate_model.py -m $writing_dir/$data/Trainedmodels/$model_name/model -d $data_dir/$data/$dataset.npz -out $writing_dir/$data/results/$model_name/$res_name
 
 
-
-
-#### Training and evaluating
-
-# data='CENPT'
+# save train predictions
+# data='Bombyx'
 # dataset='dataset'
-# model_dir='model_inception'
-# architecture='Inception.py'
-# # create output directory
-# mkdir $data/Trainedmodels/$model_dir/
-# mkdir $data/results/$model_dir/
-# # train model
-# python shared_folder/Train_model.py -arch $architecture -d shared_folder/$data/$dataset.npz -out $data/Trainedmodels/$model_dir/
+# model_name='model_Yann_original2'
+res_name='distrib_'$model_name'_train'
+python $scripts_dir/evaluate_model.py -m $writing_dir/$data/Trainedmodels/$model_name/model -d $data_dir/$data/$dataset.npz -out $writing_dir/$data/results/$model_name/$res_name -data 'train'
 
 
-# # save test predictions
-# # data='Bombyx'
-# # dataset='dataset'
-# # model_dir='model_Yann_original2'
-# res_name='distrib_'$model_dir'_test'
-# python shared_folder/evaluate_model.py -m $data/Trainedmodels/$model_dir/model -d shared_folder/$data/$dataset.npz -out $data/results/$model_dir/$res_name
+### Relabeling and re-training
+data='CENPT'
+dataset='dataset'
+model_name='model_inception'
+new_dataset='dataset_rel55'
+python $scripts_dir/relabel_data.py -m $writing_dir/$data/Trainedmodels/$model_name/model -d $data_dir/$data/$dataset.npz -out $data_dir/$data/$new_dataset -t 0.55
 
 
-# # save train predictions
-# # data='Bombyx'
-# # dataset='dataset'
-# # model_dir='model_Yann_original2'
-# res_name='distrib_'$model_dir'_train'
-# python shared_folder/evaluate_model.py -m $data/Trainedmodels/$model_dir/model -d shared_folder/$data/$dataset.npz -out $data/results/$model_dir/$res_name -data 'train'
-
-
-#### Relabeling and re-training
-# data='CENPT'
-# dataset='dataset'
-# model_dir='model_inception'
-# new_dataset='dataset_rel55'
-# python shared_folder/relabel_data.py -m $data/Trainedmodels/$model_dir/model -d shared_folder/$data/$dataset.npz -out $data/$new_dataset -t 0.55
-
-
-# data='CENPT'
-# dataset='dataset'
-# new_labels='dataset_rel5'
-# model_dir='model_inception_rel5'
-# architecture='Inception.py'
-# # create output directory
-# mkdir $data/Trainedmodels/$model_dir/
-# mkdir $data/results/$model_dir/
-# # train model
-# python shared_folder/Train_model.py -arch $architecture -d $data/$dataset.npz -out shared_folder/$data/Trainedmodels/$model_dir/ -rel $data/$new_labels.npz
-# # save test predictions
-# res_name='distrib_'$model_dir'_test_rel'
-# python shared_folder/evaluate_model.py -m $data/Trainedmodels/$model_dir/model -d shared_folder/$data/$dataset.npz -out $data/results/$model_dir/$res_name -rel $data/$new_labels.npz
-# # save train predictions
-# res_name='distrib_'$model_dir'_train_rel'
-# python shared_folder/evaluate_model.py -m $data/Trainedmodels/$model_dir/model -d shared_folder/$data/$dataset.npz -out $data/results/$model_dir/$res_name -data 'train' -rel $data/$new_labels.npz
+data='CENPT'
+dataset='dataset'
+new_labels='dataset_rel5'
+model_name='model_inception_rel5'
+architecture='Inception.py'
+# create output directory
+mkdir $writing_dir/$data/Trainedmodels/$model_name/
+mkdir $writing_dir/$data/results/$model_name/
+# train model
+python $scripts_dir/Train_model.py -arch $architecture -d $data_dir/$data/$dataset.npz -out $writing_dir/$data/Trainedmodels/$model_name/ -rel $data_dir/$data/$new_labels.npz
+# save test predictions
+res_name='distrib_'$model_name'_test_rel'
+python $scripts_dir/evaluate_model.py -m $writing_dir/$data/Trainedmodels/$model_name/model -d $data_dir/$data/$dataset.npz -out $writing_dir/$data/results/$model_name/$res_name -rel $data_dir/$data/$new_labels.npz
+# save train predictions
+res_name='distrib_'$model_name'_train_rel'
+python $scripts_dir/evaluate_model.py -m $writing_dir/$data/Trainedmodels/$model_name/model -d $data_dir/$data/$dataset.npz -out $writing_dir/$data/results/$model_name/$res_name -data 'train' -rel $data_dir/$data/$new_labels.npz
