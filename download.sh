189081619454aa1d1a1ed4ffc06087116f64e67b
Alex Westbrook <alex.e.westbrook@gmail.com>
2022-07-22 19:58:06 +0200

    minor changes

--- download.sh
+++ download.sh
@@ -46,12 +46,11 @@
 
 
 #### A549G4
-# IP
-prefetch SRR9603955
-fasterq-dump SRR9603955 --outdir ../shared_folder/A549G4/raw_data/IP/
+# # IP
+# prefetch SRR9603955
+# fasterq-dump SRR9603955 --outdir ../shared_folder/A549G4/raw_data/IP/
 prefetch SRR9603953
 fasterq-dump SRR9603953 --outdir ../shared_folder/A549G4/raw_data/IP/
-fasterq-dump SRR9603955 --outdir ../shared_folder/A549G4/raw_data/IP/
 # Control (Input)
 prefetch SRR9603956
 fasterq-dump SRR9603956 --outdir ../shared_folder/A549G4/raw_data/Control/
