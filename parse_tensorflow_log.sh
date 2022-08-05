e5603ed8dada0186f1cf54252977af188e42c382
Alexwestbrook <alex.e.westbrook@gmail.com>
2022-08-05 11:08:58 +0200

    corrections and removed auto-sharding logs

--- parse_tensorflow_log.sh
+++ parse_tensorflow_log.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-awk 'index($0, "tensorflow/core/common_runtime")==0&&index($0, "/job:localhost") {print $0}' nohup.out > parsed.txt
-awk '/^(  |\t|attr|op:|input:)/ {next} {print $0}' parsed.txt > parsed.txt
+awk 'index($0, "tensorflow/core/common_runtime")==0&&index($0, "/job:localhost")==0&&index($0, "tensorflow/core/grappler/optimizers")==0&&index($0, "options.experimental_distribute.auto_shard_policy")==0 {print $0}' nohup.out > temp.txt
+awk '/^(  |\}|attr|op:|input:)/ {next} {print $0}' temp.txt > parsed.txt
+rm temp.txt
