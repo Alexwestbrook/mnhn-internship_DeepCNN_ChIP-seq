9fb477ab5585e649c22907f26d3676269f67a43c
Alex Westbrook <alex.e.westbrook@gmail.com>
2022-10-21 16:33:07 +0200

    added shuffling of reads

--- extract_full_reads.sh
+++ extract_full_reads.sh
@@ -35,8 +35,8 @@ then
     # extract ids of reads matching subsampled sequence from at least one end
     awk -v l=$length -F ' |\t' '(length($3)==l && index($3, "N")==0) {if (FNR==NR) {ids1[$1];next;} if ($1 in ids1) {ids2[$1]}} END {for (id in ids) {print id}}' $fastq_1'_tmp' $fastq_2'_tmp' > $sequences'_ids'
     # extract fastq lines with corresponding ids in each file
-    awk -F ' |\t' 'FNR==NR{ids[$1];next;} ($1 in ids) {printf("%s %s\n%s\n%s\n%s\n",$1,$2,$3,$4,$5); }' $sequences'_ids' $fastq_1'_tmp' > $final_name'_1.fastq'
-    awk -F ' |\t' 'FNR==NR{ids[$1];next;} ($1 in ids) {printf("%s %s\n%s\n%s\n%s\n",$1,$2,$3,$4,$5); }' $sequences'_ids' $fastq_2'_tmp' > $final_name'_2.fastq'
+    awk -F ' |\t' 'FNR==NR{ids[$1];next;} ($1 in ids) {printf("%s %s\n%s\n%s\n%s\n",$1,$2,$3,$4,$5); }' $sequences'_ids' $fastq_1'_tmp' | shuf > $final_name'_1.fastq'
+    awk -F ' |\t' 'FNR==NR{ids[$1];next;} ($1 in ids) {printf("%s %s\n%s\n%s\n%s\n",$1,$2,$3,$4,$5); }' $sequences'_ids' $fastq_2'_tmp' | shuf > $final_name'_2.fastq'
     rm $sequences'_ids'
     rm $fastq_1'_tmp'
     rm $fastq_2'_tmp'
@@ -45,7 +45,7 @@ else
     # merge and linearize fastq files
     awk 'NR%4==1 {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s\t",$0);} END {printf("\n")}' $fastq_files > $final_name'_tmp'
     # extract fastq lines with sequence of correct length
-    awk -v l=$length -F '\t' '(length($2)==l && index($2, "N")==0) {printf("%s\n%s\n%s\n%s\n",$1,$2,$3,$4); }' $final_name'_tmp' > $final_name.fastq
+    awk -v l=$length -F '\t' '(length($2)==l && index($2, "N")==0) {printf("%s\n%s\n%s\n%s\n",$1,$2,$3,$4); }' $final_name'_tmp' | shuf > $final_name.fastq
     rm $final_name'_tmp'
 fi
 echo 'done'
