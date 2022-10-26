6dadbfbb8e8ec695376ae626cbcdbf18680b67fc
Alex Westbrook <alex.e.westbrook@gmail.com>
2022-10-26 16:13:45 +0200

    added read counting un fastq

--- bash_utils.sh
+++ bash_utils.sh
@@ -1,6 +1,9 @@
 # find recursively files with specific name
 find . -mindepth 2 -type f -name "*.fastq"
 
+# bash arithmetic on wc
+expr `wc -l < file` / 4
+
 # check read lengths in fastq
 awk 'NR%4 == 2 {lengths[length($0)]++ ; counter++} END {for (l in lengths) {print l, lengths[l]}; print "total reads: " counter}' $fastq
 
