53046fc93e9cfe0dcdd1c27587b4bf710dbaa7d4
Alex Westbrook <alex.e.westbrook@gmail.com>
2023-10-10 17:17:54 +0200

    added arguments maxoptions for kMC selection on a maximum number of options, extradims to add dimensions to the one-hot before input to the model. Added get_profile_mid1 function to predict with single output model in the center of the window, changed the structure of the code to use either old predict function, renamed get_profile_hint, or the new one. Changed saving by making subdirectories, energy is stored in a simpler fashion, target is savec seperately and plotted

--- kMC_sequence_design.py
+++ kMC_sequence_design.py
@@ -42,6 +42,10 @@ def parsing():
         "-ord", "--one_hot_order", type=str, default='ACGT',
         help="order of the one-hot encoding for the model")
     parser.add_argument(
+        "--extradims", type=int, nargs='+',
+        help="extra dimensions for the one-hot input of the model, which by "
+             "default is of shape (batch_size, length, 4)")
+    parser.add_argument(
         "-kfile", "--kmer_file", type=str,
         default='/home/alex/shared_folder/SCerevisiae/genome/W303/'
                 'W303_3mer_freq.csv',
@@ -74,7 +78,7 @@ def parsing():
         "-mid", "--middle_pred", action='store_true',
         help="specifies to predict only on middle window")
     parser.add_argument(
-        "-targ", "--target", type=float, nargs='+', default=[],
+        "-targ", "--target", type=float, nargs='+',
         help="target profile for the designed sequences")
     parser.add_argument(
         "-targ_gc", "--target_gc", type=float, default=0.3834,
@@ -89,6 +93,10 @@ def parsing():
         "-temp", "--temperature", type=float, default=0.1,
         help="temperature for kMC")
     parser.add_argument(
+        "-maxopt", "--max_options", type=float, default=1000,
+        help="maximum number of options to apply nonzero probability to. Only "
+             "best options are kept for selection. Set to -1 for no maximum.")
+    parser.add_argument(
         "-b", "--batch_size", type=int, default=1000,
         help="number of mutations to predict on at once")
     parser.add_argument(
@@ -101,9 +109,11 @@ def parsing():
                  args.stride, args.batch_size]:
         assert item >= 1
     assert args.temperature > 0
-    if args.target == []:
-        args.target = [0] * args.length
-    if args.start_seqs:
+    if args.target is None:
+        args.target = np.zeros(args.length)
+    else:
+        args.target = np.array(args.target, dtype=float)
+    if args.start_seqs is not None:
         seqs = np.load(args.start_seqs)
         assert seqs.ndim == 2
         args.n_seqs, args.length = seqs.shape
@@ -182,7 +192,7 @@ def all_mutations(seqs, seen_bases):
     return mutated, mut_energy
 
 
-def tf_idx_to_one_hot(idx, order='ACGT'):
+def tf_idx_to_one_hot(idx, order='ACGT', extradims=None):
     """Convert array of indexes into one-hot in tf format.
 
     Parameters
@@ -193,6 +203,9 @@ def tf_idx_to_one_hot(idx, order='ACGT'):
         String representation of the order in which to encode bases. Default
         value of 'ACGT' means that A has the representation with 1 in first
         position, C with 1 in second position, etc...
+    extradims : int or list of int
+        Extra dimensions to give to the one_hot, which by default is of shape
+        idx.shape + (4,)
 
     Returns
     -------
@@ -203,17 +216,16 @@ def tf_idx_to_one_hot(idx, order='ACGT'):
     converter = np.zeros((4, 4))
     for i, c in enumerate('ACGT'):
         converter[i, order.find(c)] = 1
-    return tf.convert_to_tensor(converter[idx], dtype=tf.float32)
+    one_hot = converter[idx]
+    if extradims is not None:
+        one_hot = np.expand_dims(one_hot, axis=extradims)
+    return tf.convert_to_tensor(one_hot, dtype=tf.float32)
 
 
-def get_profile(seqs, model, winsize, head_interval, reverse=False,
-                middle=True, batch_size=1000, one_hot_order='ACGT',
-                stride=1):
+def get_profile_hint(seqs, model, winsize, head_interval, reverse=False,
+                     middle=True, stride=1, batch_size=1000,
+                     one_hot_converter=tf_idx_to_one_hot):
     """Predict profile.
-
-    Mutation energy of a mutation X -> Y is defined as the number of times Y
-    has been seen at this position during the optimization of this particular
-    sequence.
 
     Parameters
     ----------
@@ -223,18 +235,34 @@ def get_profile(seqs, model, winsize, head_interval, reverse=False,
     model
         Tensorflow model instance supporting the predict method, with multiple
         outputs starting at first window position and with regular spacing
+    winsize : int
+        Input length of the model
+    head_interval : int
+        Spacing beatween outputs of the model
+    reverse : bool, default=False
+        If True predict on reverse strand. Default is False for predicting on
+        forward strand.
+    middle : bool, default=True
+        Whether to use only the middle half of output heads for deriving
+        predictions. This results in no predictions on sequence edges.
+    stride : int, default=1
+        Stride to use for prediction. Using a value other than 1 will result
+        in bases being skipped and make prediction faster
+    batch_size : int, default=1000
+        Number of full sequences to feed to model.predict() at once. This is
+        different from the model batch_size, which is derived inside
+        model.predict(). The constraint here is the total number of one-hot
+        encoded sequence slides that can fit into memory.
+    one_hot_converter : function
+        Function taking as input an array of indexes into 'ACGT' with shape
+        (n_windows, window_length) and converts it to the required model input
+        format.
 
     Returns
     -------
-    mutated : ndarray, shape=(n, 3*l, l)
-        Array of all possible mutations of each sequence in seqs
-    mut_energy : ndarray, shape=(n, 3*l)
-        Array of mutation energy associated with each mutation
-
-    Notes
-    -----
-    Mutations are performed in a circular rotation (0 -> 1 -> 2 -> 3 -> 0) by
-    adding 1, 2 or 3 to the index and then taking the modulo 4.
+    ndarray
+        Array of predictions with same shape as seqs, except on the last
+        dimension, containing predictions for that sequence.
     """
     # Copy sequence array and make 2D with sequence along the 2nd axis
     seqs2D = seqs.reshape(-1, seqs.shape[-1]).copy()
@@ -261,26 +289,19 @@ def get_profile(seqs, model, winsize, head_interval, reverse=False,
         # each time
         start = np.random.randint(0, stride)
         print(f'Predicting with stride {stride} and offset {start}')
-        # Determine number of slides for reshaping, and allow higher batch_size
-        slides_per_seq = head_interval // stride
+        # Allow higher batch_size
         batch_size *= stride
-        # Get windows to predict on
-        slides = utils.strided_sliding_window_view(
-            seqs2D[:, start:],
-            (len(seqs2D), winsize),
-            stride=(jump_stride, stride),
-            sliding_len=head_interval,
-            axis=-1).reshape(-1, slides_per_seq, winsize)
     else:
-        # Determine number of slides for reshaping
-        slides_per_seq = head_interval
-        # Get windows to predict on
-        slides = utils.strided_sliding_window_view(
-            seqs2D,
-            (len(seqs2D), winsize),
-            stride=jump_stride,
-            sliding_len=head_interval,
-            axis=-1).reshape(-1, slides_per_seq, winsize)
+        start = 0
+    # Determine number of slides for reshaping
+    slides_per_seq = head_interval // stride
+    # Get windows to predict on
+    slides = utils.strided_sliding_window_view(
+        seqs2D[:, start:],
+        (len(seqs2D), winsize),
+        stride=(jump_stride, stride),
+        sliding_len=head_interval,
+        axis=-1).reshape(-1, slides_per_seq, winsize)
     # Initialise predictions list
     preds = []
     # Process slides by batch because it may use to much memory, batching is
@@ -290,7 +311,7 @@ def get_profile(seqs, model, winsize, head_interval, reverse=False,
                           np.arange(batch_size, len(slides), batch_size)):
         # Convert to one-hot and predict
         pred = model.predict(
-            tf_idx_to_one_hot(split.reshape(-1, winsize), order=one_hot_order)
+            one_hot_converter(split.reshape(-1, winsize))
         ).squeeze()
         # Collect garbage to prevent memory leak from model.predict()
         gc.collect()
@@ -301,6 +322,89 @@ def get_profile(seqs, model, winsize, head_interval, reverse=False,
         pred = np.transpose(pred.reshape(-1, slides_per_seq, kept_heads),
                             [0, 2, 1])
         pred = pred.reshape(-1, slides_per_seq*kept_heads)
+        preds.append(pred)
+    # Concatenate predictions and reshape as original sequences
+    preds = np.concatenate(preds).reshape(seqs.shape[:-1] + (-1,))
+    # Maybe reverse predictions
+    if reverse:
+        preds = np.flip(preds, axis=-1)
+    return preds
+
+
+def get_profile_mid1(seqs, model, winsize, reverse=False, stride=1,
+                     batch_size=1000, one_hot_converter=tf_idx_to_one_hot):
+    """Predict profile.
+
+    Parameters
+    ----------
+    seqs : ndarray
+        Array of indexes into 'ACGT', sequences to predict on are read on the
+        last axis
+    model
+        Tensorflow model instance supporting the predict method, with a single
+        output in the middle of the window.
+    winsize : int
+        Input length of the model
+    reverse : bool, default=False
+        If True predict on reverse strand. Default is False for predicting on
+        forward strand.
+    stride : int, default=1
+        Stride to use for prediction. Using a value other than 1 will result
+        in bases being skipped and make prediction faster
+    batch_size : int, default=1000
+        Number of full sequences to feed to model.predict() at once. This is
+        different from the model batch_size, which is derived inside
+        model.predict(). The constraint here is the total number of one-hot
+        encoded sequence slides that can fit into memory.
+    one_hot_converter : function
+        Function taking as input an array of indexes into 'ACGT' with shape
+        (n_seqs, seq_length) and converts it to the required model input
+        format.
+
+    Returns
+    -------
+    ndarray
+        Array of predictions with same shape as seqs, except on the last
+        dimension, containing predictions for that sequence.
+    """
+    # Copy sequence array and make 2D with sequence along the 2nd axis
+    seqs2D = seqs.reshape(-1, seqs.shape[-1]).copy()
+    # Maybe reverse complement the sequences
+    if reverse:
+        seqs2D[seqs2D == 0] = -1
+        seqs2D[seqs2D == 3] = 0
+        seqs2D[seqs2D == -1] = 3
+        seqs2D[seqs2D == 1] = -1
+        seqs2D[seqs2D == 2] = 1
+        seqs2D[seqs2D == -1] = 2
+        seqs2D = np.flip(seqs2D, axis=-1)
+    if stride != 1:
+        # Do not predict all bases, but randomize which ones are predicted
+        # each time
+        start = np.random.randint(0, stride)
+        print(f'Predicting with stride {stride} and offset {start}')
+        # Allow higher batch_size
+        batch_size *= stride
+    else:
+        start = 0
+    # Get windows to predict on
+    slides = utils.strided_window_view(
+        seqs2D[:, start:],
+        (len(seqs2D), winsize),
+        (1, stride)).transpose([0, 2, 1, 3]).reshape(-1, winsize)
+    # Initialise predictions list
+    preds = []
+    # Process slides by batch because it may use to much memory, batching is
+    # done only along first dimension because all slides_per_seq must be
+    # processed together
+    for split in np.split(slides,
+                          np.arange(batch_size, len(slides), batch_size)):
+        # Convert to one-hot and predict
+        pred = model.predict(
+            one_hot_converter(split.reshape(-1, winsize))
+        ).squeeze()
+        # Collect garbage to prevent memory leak from model.predict()
+        gc.collect()
         preds.append(pred)
     # Concatenate predictions and reshape as original sequences
     preds = np.concatenate(preds).reshape(seqs.shape[:-1] + (-1,))
@@ -327,7 +431,7 @@ def rmse(y_true, y_pred):
     return np.sqrt(np.mean((y_true - y_pred)**2, axis=-1))
 
 
-def select(energies, weights, temperature, step=None):
+def select(energies, weights, temperature, step=None, maxoptions=1000):
     """Choose a mutation with low energy based on kMC method.
 
     Parameters
@@ -342,6 +446,8 @@ def select(energies, weights, temperature, step=None):
     step : int, default=None
         Step index in the optimisation process. If set, the computed
         probabilities will be save to a file.
+    maxoptions : int, default=1000
+        Maximum number of options to give nonzero probabilities to.
 
     Returns
     -------
@@ -359,10 +465,13 @@ def select(energies, weights, temperature, step=None):
     """
     # Compute energy and probability
     tot_energy = sum(w*e for w, e in zip(weights, energies))
+    if maxoptions != -1:
+        thres = np.partition(tot_energy, maxoptions, axis=-1)[:, maxoptions]
+        tot_energy[tot_energy > np.expand_dims(thres, axis=-1)] = np.inf
     prob = utils.exp_normalize(-tot_energy / temperature)
     # Maybe save probabilities
     if step is not None:
-        np.save(Path(args.output_dir, f'prob_step{step}.npy'), prob)
+        np.save(Path(args.output_dir, "probs", f'prob_step{step}.npy'), prob)
     # Select by the position of a random number in the cumulative sum
     cumprob = np.cumsum(prob, axis=-1)
     r = np.random.rand(len(prob)).reshape(-1, 1)
@@ -387,8 +496,30 @@ def main(args):
             args.model_file,
             custom_objects={'correlate': correlate, 'mae_cor': mae_cor})
     winsize = model.input_shape[1]
-    head_interval = winsize // model.output_shape[1]
-    assert head_interval % args.stride == 0
+
+    # Tune predicting functions according to args
+
+    def one_hot_converter(idx):
+        return tf_idx_to_one_hot(
+            idx, order=args.one_hot_order, extradims=args.extradims)
+
+    if model.output_shape[1] == 1:
+
+        def get_profile(seqs, reverse=False):
+            return get_profile_mid1(
+                seqs, model, winsize, reverse=reverse, stride=args.stride,
+                batch_size=args.batch_size,
+                one_hot_converter=one_hot_converter)
+    else:
+        head_interval = winsize // model.output_shape[1]
+        assert head_interval % args.stride == 0
+
+        def get_profile(seqs, reverse=False):
+            return get_profile_hint(
+                seqs, model, winsize, head_interval, reverse=reverse,
+                middle=args.middle, stride=args.stride,
+                batch_size=args.batch_size,
+                one_hot_converter=one_hot_converter)
     # Generate and save start sequences
     if args.seed != -1:
         np.random.seed(args.seed)
@@ -399,7 +530,7 @@ def main(args):
                                 index_col=[i for i in range(args.k)])
         seqs = utils.random_sequences(args.n_seqs, args.length,
                                       freq_kmer['W303'], out='idx')
-        np.save(Path(args.output_dir, "start_seqs.npy"), seqs)
+        np.save(Path(args.output_dir, "designed_seqs", "start_seqs.npy"), seqs)
     # Initialize array of already seen bases for each position
     seen_bases = np.eye(4, dtype=int)[seqs]
     # Determine rows and cols to place n_seqs subplots
@@ -415,14 +546,8 @@ def main(args):
         # Generate all mutations, and associated mutation energy
         seqs, mut_energy = all_mutations(seqs, seen_bases)
         # Predict on forward and reverse strands
-        preds = get_profile(
-            seqs, model, winsize, head_interval, batch_size=args.batch_size,
-            stride=args.stride, one_hot_order=args.one_hot_order,
-            middle=args.middle_pred)
-        preds_rev = get_profile(
-            seqs, model, winsize, head_interval, batch_size=args.batch_size,
-            stride=args.stride, one_hot_order=args.one_hot_order,
-            middle=args.middle_pred, reverse=True)
+        preds = get_profile(seqs)
+        preds_rev = get_profile(seqs, reverse=True)
         # Compute energy components
         gc_energy = GC_energy(seqs, args.target_gc)
         for_energy = args.loss(args.target[:preds.shape[-1]], preds)
@@ -430,17 +555,21 @@ def main(args):
         energy_list = [gc_energy, for_energy, rev_energy, mut_energy]
         # Choose best mutation by kMC method
         sel_idx, sel_energies = select(energy_list, args.weights,
-                                       args.temperature, step=step)
+                                       args.temperature, step=step,
+                                       maxoptions=args.max_options)
         # Keep selected sequence and increment seen_bases
         seqs = seqs[np.arange(len(seqs)), sel_idx]
         seen_bases[np.arange(len(seqs)),
                    sel_idx // 3,
                    seqs[np.arange(len(seqs)), sel_idx // 3]] += 1
         # Save sequence, energy and plot profile
-        np.save(Path(args.output_dir, f"mut_seqs_step{step}.npy"), seqs)
+        np.save(Path(args.output_dir, "designed_seqs",
+                     f"mut_seqs_step{step}.npy"),
+                seqs)
         with open(Path(args.output_dir, 'energy.txt'), 'a') as f:
-            f.write(f'{sel_energies}\n')
-        fig, axes = plt.subplots(nrow, ncol, figsize=(2+3*nrow, 1+2*ncol),
+            np.savetxt(f, sel_energies, fmt='%-8e',
+                       delimiter='\t', header=f'step{step}')
+        fig, axes = plt.subplots(nrow, ncol, figsize=(2+3*ncol, 1+2*nrow),
                                  facecolor='w', layout='tight', sharey=True)
         offset = head_interval - args.stride
         if args.n_seqs == 1:
@@ -461,7 +590,8 @@ def main(args):
                 ax.plot(np.arange(offset, len(prev)+offset),
                         prev, label='reverse', alpha=0.8)
                 ax.legend()
-        fig.savefig(Path(args.output_dir, f"mut_preds_step{step}.png"),
+        fig.savefig(Path(args.output_dir, "pred_figs",
+                         f"mut_preds_step{step}.png"),
                     bbox_inches='tight')
         plt.close()
         print(time.time() - t0)
@@ -475,8 +605,11 @@ if __name__ == "__main__":
     args = parsing()
     # Build output directory and initialize energy file
     Path(args.output_dir).mkdir(parents=True, exist_ok=False)
+    Path(args.output_dir, 'pred_figs').mkdir()
+    Path(args.output_dir, 'designed_seqs').mkdir()
+    Path(args.output_dir, 'probs').mkdir()
     with open(Path(args.output_dir, 'energy.txt'), 'w') as f:
-        f.write('total_energy\t'
+        f.write('# total_energy\t'
                 'gc_energy\t'
                 'for_energy\t'
                 'rev_energy\t'
@@ -487,21 +620,27 @@ if __name__ == "__main__":
     with open(Path(args.output_dir, 'config.txt'), 'w') as f:
         json.dump(to_serealize, f, indent=4)
         f.write('\n')
-        f.write(f'target: {args.target}\n')
         f.write(f'weights: {args.weights}\n')
         f.write(f'timestamp: {tmstmp}\n')
         f.write(f'machine: {socket.gethostname()}\n')
     # Convert to non json serializable objects
-    args.target = np.array(args.target, dtype=float)
     losses = {'rmse': rmse}
     args.loss = losses[args.loss]
     args.weights = np.array(args.weights, dtype=float)
+    # Save and plot target
+    np.save(Path(args.output_dir, 'target.npy'),
+            args.target)
+    fig, ax = plt.subplots(1, 1, facecolor='w', layout='tight')
+    ax.plot(args.target)
+    fig.savefig(Path(args.output_dir, f"target.png"),
+                bbox_inches='tight')
+    plt.close()
     # Start computations, save total time even if there was a failure
     try:
         main(args)
     except KeyboardInterrupt:
         with open(Path(args.output_dir, 'config.txt'), 'a') as f:
-            f.write('Interrupted\n')
+            f.write('KeyboardInterrupt\n')
             f.write(f'total time: {time.time() - t0}\n')
         raise
     with open(Path(args.output_dir, 'config.txt'), 'a') as f:
